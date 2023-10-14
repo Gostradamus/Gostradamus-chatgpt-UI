daf7b3baaab0dcb349df7b67b76d9b59b9dd67fd
Gostradamus <125901801+Gostradamus@users.noreply.github.com>
2023-10-14 20:38:27 +0000

    Update INSTALL file

--- src/token.js
+++ src/token.js
@@ -1,2 +1,6 @@
 
-/
+/*
+ This tokenizer is basically a buffering tokenizer that makes sure tokens coming in from the API
+ that are important for "content containerization" such as:
+ 
+ - three-backticks (codebl
