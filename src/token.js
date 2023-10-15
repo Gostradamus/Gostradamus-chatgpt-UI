a9b37c15fdd51b085c11d1308182446a3f81e917
Gostradamus <125901801+Gostradamus@users.noreply.github.com>
2023-10-15 06:03:38 +0000

    xhci: Fix isoc TD encoding.

--- src/token.js
+++ src/token.js
@@ -3,4 +3,8 @@
  This tokenizer is basically a buffering tokenizer that makes sure tokens coming in from the API
  that are important for "content containerization" such as:
  
- - three-backticks (codebl
+ - three-backticks (codeblocks)
+ - single-backtick (codespans)
+ - newlines (paragraphs) 
+
+ are given to the UI layer as single complete tokens. This enables easier token handling logic within the 
